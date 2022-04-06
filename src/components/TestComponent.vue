7e033b64080f259bd569c3828ac2b5cfa42f6888
SangBeom-Lee <bumeeya2@naver.com>
2022-04-06 21:19:47 +0900

    vue3 맛보기

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    text : {{ text }}
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TestComponent',
+  props: {
+    text: {
+      type: String,
+      default: () => 'abcd'
+    }
+  },
+  data () {
+    return {
+      counter: 5
+    }
+  }
+})
+</script>
---
+++ src/pages/TestVue.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page padding>
+    <q-card>
+      <q-card-section>
+        text: {{ addedText }}
+      </q-card-section>
+      <q-card-section />
+      <q-card-actions>
+        <q-btn
+          flat
+          label="add"
+          @click="add"
+        />
+      </q-card-actions>
+      <q-card-section>
+        compo....
+        <TestComponent :text="text" />
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue'
+import TestComponent from 'src/components/TestComponent.vue'
+
+export default defineComponent({
+  name: 'TestVue',
+  components: { TestComponent },
+  setup () {
+    const text = ref('abc')
+    const addedText = computed(() => text.value + ' added')
+    const add = () => {
+      text.value += 'add'
+    }
+    return {
+      text,
+      addedText,
+      add
+    }
+  }
+})
+</script>
