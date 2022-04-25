2653ca09121522fef4d6d07150ca669f90487a19
SangBeom-Lee <bumeeya2@naver.com>
2022-04-25 21:43:28 +0900

    실시간 데이터베이스 테스트

--- src/boot/firebase.ts
+++ src/boot/firebase.ts
@@ -4,8 +4,10 @@ import { initializeApp } from 'firebase/app'
 import firebaseConfig from '../../firebaseConfig'
 import { getAuth, connectAuthEmulator } from 'firebase/auth'
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
+import { getDatabase, connectDatabaseEmulator } from 'firebase/database'
+import firebaseJson from '../../firebase.json'
 
-initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig)
 
 const auth = getAuth()
 auth.useDeviceLanguage()
@@ -14,4 +16,7 @@ connectAuthEmulator(auth, 'http://localhost:9099')
 const db = getFirestore()
 connectFirestoreEmulator(db, 'localhost', 8081)
 
-export { auth, db }
+const rtdb = getDatabase(app)
+connectDatabaseEmulator(rtdb, 'localhost', firebaseJson.emulators.database.port)
+
+export { app, auth, db, rtdb }
--- src/pages/TestVue.vue
+++ src/pages/TestVue.vue
@@ -1,9 +1,10 @@
 <script setup lang='ts'>
-
+import TestRtdb from '../components/TestRtdb.vue'
 </script>
 
 <template>
   <div class="text-h1">
     new
   </div>
+  <TestRtdb />
 </template>
